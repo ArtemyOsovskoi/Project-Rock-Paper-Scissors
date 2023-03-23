4c05ad569cbd9145be8fc3049b4c9f18ce480049
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2023-03-23 15:22:15 +0300

    Made a few changes and now it's almost working

--- script.js
+++ script.js
@@ -4,49 +4,42 @@ function getComputerChoice() {
     return randomChoices;
 }
 //console.log(getComputerChoice())
-//keep score
-
-    let playerScore = 0;
-    let computerScore = 0;
 function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore++; 
+        
         return("Player won! Rock beats Scissors");
     } 
     else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore++;
+       
         return "Computer won! Paper beats Rock"
     } 
     else if (playerSelection === "rock" && computerSelection === "rock") {
-        computerScore++;
-        playerScore++;
+       
         return "Draw!"
     } 
     else if(playerSelection === "paper" && computerSelection === "scissors"){
-         computerScore++;
+        
         return "Computer won! Scissors beats Paper"
     } 
     else if(playerSelection === "paper" && computerSelection === "paper") {
-        computerScore++;
-        playerScore++;
+       
         return "Draw!"
     } 
     else if(playerSelection === "paper" && computerSelection === "rock") {
-        playerScore++; 
+       
         return "Player won! Paper beats Rock"
     } 
     else if(playerSelection === "scissors" && computerSelection === "scissors" ){
-        computerScore++;
-        playerScore++;
+     
         return "Draw!"
     } 
     else if(playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore++; 
+       
         return "Player won! Scissors beats Paper"
     } 
     else if(playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore++;
+       
         return "Computer won! Rock beats Scissors"
     } 
     else {
@@ -54,29 +47,42 @@ function playRound(playerSelection, computerSelection) {
     } 
 };
 
-const playerSelection = prompt("Rock, Paper, Scissors?").toLowerCase();
+let playerScore = 0;
+let computerScore = 0;
+
+const playerSelection = prompt("Let,s go! Rock, Paper, Scissors?").toLowerCase();
 let computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 
 function game() {
-    let roundScore = playRound();
-
+    let roundScore = playRound(playerSelection, computerSelection);
+    console.log(roundScore)
+    
     for (let i = 0; i < 5; i++) { 
         playRound()
-        if (roundScore === "Player won! Rock beats Scissors" || roundScore === "Player won! Paper beats Rock" || roundScore === "Player won! Scissors beats Paper") {
-        playerScore++; 
-        } else if (roundScore === "Computer won! Paper beats Rock" || roundScore ===  "Computer won! Scissors beats Paper" || roundScore === "Computer won! Rock beats Scissors") {
-        computerScore++;
-        }    
+        let playerInput = prompt("Rock, Paper, Scissors?").toLowerCase;
+        console.log(roundScore)
+            if (roundScore === "Player won! Rock beats Scissors" || roundScore === "Player won! Paper beats Rock" || roundScore === "Player won! Scissors beats Paper") {
+            alert("Player won! Computer lose.");
+            playerScore++;
+            } else if (roundScore === "Computer won! Paper beats Rock" || roundScore ===  "Computer won! Scissors beats Paper" || roundScore === "Computer won! Rock beats Scissors") {
+            alert("Computer won! You lose.");
+            computerScore++;
+            } else if (roundScore === "Draw!") {
+            alert("Draw!");
+            } else {
+            alert("Wrong choice.");
+            } 
+    }
     
-        if (playerScore > computerScore) {
-        alert("Player won! Computer lose...");
-        } else if (computerScore > playerScore) {
-        alert("Computer won! You lose...");
-        } else {
-        alert("Draw! Try again");
-        }
+    if (playerScore > computerScore) {
+        alert("Player won!!! Computer lose...");
+    } else if (computerScore > playerScore) {
+        alert("Computer won!!! You lose...");
+    } else if (computerScore === playerScore && computerScore !=0 && playerScore !=0 ) {
+        alert("Draw!!! Try again...");
+    } else {
+        alert("Wrong choice, please try again");
     }
 }
 console.log(game())
-
