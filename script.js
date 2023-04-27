f8b6e5e32e2c6ba2ab15e87969400fdc69466c0f
Artemy <116560582+ArtemyOsovskoi@users.noreply.github.com>
2023-04-27 17:30:36 +0300

    New logic working with DOM: now it's able to play 1 round by clicking buttons

--- script.js
+++ script.js
@@ -1,56 +1,65 @@
+/* let playerScore = 0;
+let computerScore = 0;
+let ties = 0; */
+//Add an event listener to the buttons that call   playRound function 
+//with the correct playerSelection every time a button is clicked. 
+let computerSelection = getComputerChoice();
+
 function getComputerChoice() {  
     const choices = ["rock", "paper", "scissors"];
     let randomChoices = choices[Math.floor(Math.random()*choices.length)]; 
     return randomChoices;
-}
-//console.log(getComputerChoice())
+};
+
+let buttonRock = document.querySelector("#brock");
+buttonRock.addEventListener("click", () => {
+    playRound("rock", computerSelection);
+}); 
+
+let buttonPaper = document.querySelector("#bpaper");
+buttonPaper.addEventListener("click", () => {
+    playRound("paper", computerSelection);
+}); 
+
+let buttonScissors = document.querySelector("#bscissors");
+buttonScissors.addEventListener("click", () => {
+    playRound("scissors", computerSelection);
+}); 
+
+
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection === "rock" && computerSelection === "scissors") {
-        
-        return "Player won! Rock beats Scissors"
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        alert("Player won! Rock beats Scissors");
     } 
-    else if (playerSelection === "rock" && computerSelection === "paper") {
-       
-        return "Computer won! Paper beats Rock"
+    else if (playerSelection == "rock" && computerSelection == "paper") {
+        alert("Computer won! Paper beats Rock");
     } 
-    else if (playerSelection === "rock" && computerSelection === "rock") {
-       
-        return "Draw!"
+    else if (playerSelection == "rock" && computerSelection == "rock") {
+        alert("Draw! Rock vs Rock");
     } 
     else if(playerSelection === "paper" && computerSelection === "scissors"){
-        
-        return "Computer won! Scissors beats Paper"
+        alert("Computer won! Scissors beats Paper");
     } 
-    else if(playerSelection === "paper" && computerSelection === "paper") {
-       
-        return "Draw!"
+    else if(playerSelection === "paper" && computerSelection === "paper") {     
+        alert("Draw! Paper vs Paper");
     } 
     else if(playerSelection === "paper" && computerSelection === "rock") {
-       
-        return "Player won! Paper beats Rock"
+        alert("Player won! Paper beats Rock");
     } 
     else if(playerSelection === "scissors" && computerSelection === "scissors" ){
-     
-        return "Draw!"
+        alert("Draw! Scissors vs Scissors");
     } 
     else if(playerSelection === "scissors" && computerSelection === "paper") {
-       
-        return "Player won! Scissors beats Paper"
+        alert("Player won! Scissors beats Paper");
     } 
     else if(playerSelection === "scissors" && computerSelection === "rock") {
-       
-        return "Computer won! Rock beats Scissors"
-    } 
-    else {
-       return "Wrong choice, please choose between rock, paper or scissors";
+        alert("Computer won! Rock beats Scissors");
     } 
 };
 
-let playerScore = 0;
-let computerScore = 0;
 
-function game() {    
+ /* function game() {    
     for (let i = 0; i < 5; i++) { 
         const playerSelection = prompt("Please choose - Rock, Paper, Scissors?").toLowerCase(); //player move
         let computerSelection = getComputerChoice();//computer move
@@ -69,7 +78,7 @@ function game() {
             } else {
             alert("Wrong choice.");
             } 
-    }
+    } 
     //final evaluation and declaring a winner and loser
     if (playerScore > computerScore) {
         alert("Player won!!! Computer lose..."+"Player"+":"+ playerScore + "-" + "Computer"+":"+computerScore);
@@ -80,5 +89,5 @@ function game() {
     } else {
         alert("Wrong choice, please try again");
     }
-}
-console.log(game())
+} 
+ */
